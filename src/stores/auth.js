576ee9b61f3eea42b17f1b719f7da6aa13e73028
MREZA <rezaworku31@gmail.com>
2025-12-05 00:05:13 +0700

    created : created FE for ticktrack

---
+++ src/stores/auth.js
@@ -0,0 +1,85 @@
+// TODO: Import necessary dependencies
+// Hint: You'll need pinia, axios, router, and cookies
+import { axiosInstance } from "@/plugins/axios"
+import router from "@/router"
+import { defineStore } from "pinia"
+import { handleError } from "vue"
+import Cookies from "js-cookie"
+
+export const useAuthStore = defineStore("auth", {
+    state: () => ({
+        user: null,
+        loading: null,
+        error: null,
+        success: null,
+    }),
+
+    getters: {
+        // TODO: Implement token getter
+        // Hint: Get token from cookies
+        token: () => Cookies.get('token'),
+        isAuthenticated: (state) => !!state.user,
+
+        // TODO: Implement isAuthenticated getter
+        // Hint: Check if user exists
+    },
+
+    actions: {
+        async login(credentials) {
+            // TODO: Implement login action
+            // Steps:
+            // 1. Set loading state 
+            this.loading = true
+            // 2. Make API call to login endpoint
+            try {
+                const response = await axiosInstance.post('/login', credentials)
+                const token = response.data.data.token
+
+                Cookies.set('token', token)
+
+                this.success = response.data.message
+                if (response.data.data.user.role == "admin") {
+                    router.push({ name: 'admin.dashboard' })
+                } else {
+                    router.push({ name: 'app.dashboard' })
+                }
+            } catch (error) {
+                this.error = error.response?.data?.message
+            } finally {
+                this.loading = false
+            }
+            // 3. Store token in cookies
+            // 4. Handle success/error
+            // 5. Redirect user
+        },
+
+        async register(credentials) {
+            // TODO: Implement register action
+            // Steps:
+            // 1. Set loading state
+            // 2. Make API call to register endpoint
+            // 3. Store token in cookies
+            // 4. Handle success/error
+            // 5. Redirect user
+        },
+
+        async logout() {
+            // TODO: Implement logout action
+            // Steps:
+            // 1. Set loading state
+            // 2. Make API call to logout endpoint
+            // 3. Remove token from cookies
+            // 4. Clear user state
+            // 5. Redirect to login
+        },
+
+        async checkAuth() {
+            // TODO: Implement checkAuth action
+            // Steps:
+            // 1. Set loading state
+            // 2. Make API call to get user data
+            // 3. Update user state
+            // 4. Handle unauthorized error
+        }
+    },
+}) 
